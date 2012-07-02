9284a7522ef80295d9ce7704b29dc10408f45590
Andraz Brodnik <brodul@brodul.org>
2012-07-02 16:43:17 +0200

    Adding basic settings for pelican.

---
+++ pelican.conf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author's name
+AUTHOR = u'Andraž brodul Brodnik'
+SITENAME = "brodulog (BETA)"
+
+# Dir with input files
+PATH = './content/'
+TIMEZONE = 'CET'
+
+# Extras
+GITHUB_URL = 'http://github.com/brodul/blog.brodul.org'
+TWITTER_USERNAME = 'brodul'
+DISQUS_SITENAME = 'brodulog'
+GOOGLE_ANALYTICS = 'UA-33078920-1'
+
+# Social links
+SOCIAL = (('twitter', 'http://twitter.com/brodul'),
+          ('github', 'http://github.com/brodul'),)
+
+

826e65e198b8e37fe5aad1547e3832817cb92fa4
Andraz Brodnik <brodul@brodul.org>
2012-08-18 20:16:42 +0200

    Minor fix in fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from fabric.api import run, local, task, env
-from fabric.context_managers import cd
+from fabric.context_managers import lcd
 from fabric.colors import blue, red, green
 from fabric.operations import put
 
@@ -19,7 +19,7 @@ def local_server_deploy():
     """docstring for local_server_deploy"""
     local('mkdir -p %(building_dir)s' % env)
 
-    with cd(env.building_dir):
+    with lcd(env.building_dir):
         local('git clone -b %(branch)s %(repository)s .' % env)
         local('python bootstrap.py -d')
         local('bin/buildout')
